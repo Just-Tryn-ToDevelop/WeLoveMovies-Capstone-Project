4a4b0f19a916c58243c2ad23d742492a2bacc532
Ronald Justyn Ward <justlivin91@gmail.com>
2022-11-13 22:24:05 -0600

    put app.listen in a different place within the code in server.js

--- Project/src/server.js
+++ Project/src/server.js
@@ -4,12 +4,12 @@ const app = require("./app");
 const knex = require("./db/connection");
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
+    app.listen(PORT, listener);
   })
   .catch((error) => {
     console.error(error);
